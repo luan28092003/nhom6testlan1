bf1cf553704337cd18d7b8bfcab66835b68b4643
LuanNT <thanhluan2818@gmail.com>
2022-11-10 15:45:56 +0700

    luan test lan 1

---
+++ app/src/main/java/luanntph26376/fpoly/nhom6testlan1/MainActivity.java
@@ -0,0 +1,14 @@
+package luanntph26376.fpoly.nhom6testlan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
